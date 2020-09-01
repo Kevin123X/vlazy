0817bfb4e0e32bfb0549b9b882aca5231c7fb2fb
Kevin <liukaiwen-cn@outlook.com>
2020-09-01 13:33:51 +0800

    🎨 IMPROVE

--- vue.config.js
+++ vue.config.js
@@ -1,45 +1,11 @@
-'use strict'
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following methods:
-// port = 9999 npm run dev OR npm run dev --port = 9999
-
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
+const env = process.env.NODE_ENV;
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: '/',
-  outputDir: 'docs',
-  // assetsDir: 'static',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
   devServer: {
     port: 8080,
     open: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    }
   },
-  chainWebpack(config) {
-    // it can improve the speed of the first screen, it is recommended to turn on preload
-    config.plugin('preload').tap(() => [
-      {
-        rel: 'preload',
-        // to ignore runtime.js
-        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-        include: 'initial'
-      }
-    ])
-
-    // when there are many pages, it will cause too many meaningless requests
-    config.plugins.delete('prefetch')
-  }
-}
+  publicPath: env === "production" ? "./" : "",
+  indexPath: "index.html",
+  outputDir: "docs",
+  assetsDir: "",
+};
